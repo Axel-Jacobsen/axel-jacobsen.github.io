cf17596d9c378c8c9e55176a087d18d8448cd507
Axel Jacobsen <axelnj44@gmail.com>
2023-11-19 18:29:18 -0800

    template and compile

--- compile_paragraphs.py
+++ compile_paragraphs.py
@@ -20,21 +20,44 @@ def process_contents(contents: str) -> str:
     return pypandoc.convert_text(contents, "html", format="md")
 
 
+def titles_to_list(titles: list[tuple[str, Path]]) -> str:
+    links = [
+        f"<li> <a href=paragraphs/{path.stem}>{title}</a> </li>"
+        for title, path in titles
+    ]
+
+    html_formatted = "\n".join(links)
+    return f"<ul>\n{html_formatted}\n</ul>"
+
+
+def write_html_doc(title: str, contents: str, html_path: Path):
+    new_doc = (
+        template
+            .replace("{{title}}", title)
+            .replace("{{content}}", contents_html_formatted)
+    )
+
+    with open(html_path, "w") as g:
+        g.write(new_doc)
+
+
 if __name__ == "__main__":
     template = load_template()
+    titles_and_links: list[tuple[str, Path]] = []
 
     for md_path in PARAGRAPHS_DIR.glob("*.md"):
-
         with open(md_path) as f:
             title = f.readline().strip().replace("# ", "")
-            contents = f.read().strip().replace("\n", "\n\n")
-            contents_html_formatted = process_contents(contents)
+            contents = f.read().strip()
 
-        new_doc = (
-            template
-                .replace("{{title}}", title)
-                .replace("{{content}}", contents_html_formatted)
-        )
+        contents_html_formatted = process_contents(contents)
 
-        with open(md_path.with_suffix(".html"), "w") as g:
-            g.write(new_doc)
+        html_path = md_path.with_suffix(".html")
+        titles_and_links.append((title, html_path))
+        write_html_doc(title, contents_html_formatted, html_path)
+
+    paragraphs_title = "paragraphs<br><a href='..'>home</a>"
+    paragraphs_contents = titles_to_list(titles_and_links)
+    write_html_doc(
+        paragraphs_title, paragraphs_contents, PARAGRAPHS_DIR / "paragraphs.html"
+    )
